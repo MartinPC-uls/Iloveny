ad7dd7e0d410d6aa5502b3734baa3315cc0cb2b9
RobertoVerdugo <roberto.verdugo@userena.cl>
2021-11-04 11:14:12 -0300

    Clase conexion para postgresql

---
+++ src/a/Modelo/Conexion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a.Modelo;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Roberto
+ */
+public class Conexion {
+    private final String base ="iloveny";
+    private final String user = "iloveny_dev";
+    private final String password = "eFgq5kq";
+    private final String url = "jdbc:postgresql://10.4.3.195:5432/"+base;
+    private Connection con = null;
+    
+    public  Connection conectar(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+    public void desconectar(){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+}
