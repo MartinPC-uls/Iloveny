6795dcb8efb934b506f44b91a34fb569f3c249e7
Felipe Barrientos Rojas <felipeignacio.barrientos@userena.cl>
2021-11-04 11:49:31 -0300

    Try

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -3,6 +3,8 @@ package vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
 
 public class Login {
 
@@ -27,8 +29,17 @@ public class Login {
 
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 889, 547);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 255, 0));
+		panel.setBounds(0, 0, 313, 508);
+		frame.getContentPane().add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(311, 0, 562, 508);
+		frame.getContentPane().add(panel_1);
 	}
-
 }
