cfd4bc17022c5da39aa09c68d58a7ae4c7eecd13
MartinPC-uls <martin.pizarro@userena.cl>
2021-11-05 20:02:35 -0300

    Agregado el inicializador para el Login.

---
+++ src/vista/Inicializador.java
@@ -0,0 +1,11 @@
+package vista;
+
+public class Inicializador {
+	
+	private static Login login = new Login();
+	
+	public static void main(String[] args) {
+		login.frame.setVisible(true);
+	}
+	
+}
