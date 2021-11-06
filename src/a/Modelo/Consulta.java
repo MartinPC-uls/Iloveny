5bb7bf6280af2a612b049dddb37e237099113f54
MartinPC-uls <martin.pizarro@userena.cl>
2021-11-06 05:42:48 -0300

    fix agregado a verificarAdmin.

--- src/a/Modelo/Consulta.java
+++ src/a/Modelo/Consulta.java
@@ -229,7 +229,7 @@ public class Consulta extends Conexion{
         PreparedStatement ps;
         ResultSet rs;
         Connection con = conectar();
-        String sql = "SELECT * FROM administrador WHERE Usuario=? AND Contrasena=?";
+        String sql = "SELECT * FROM administrador WHERE usuario=? AND contrasena=?";
         try {
             ps = con.prepareStatement(sql);
             ps.setString(1, Usuario);
